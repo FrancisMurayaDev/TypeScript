3390d1778a5b8b313af7891f82c6095318d2d557
FrancisMurayaDev <francismurayadev@gmail.com>
2025-03-15 13:41:18 +0300

    finsih learning typescript types

---
+++ Tests/index.js
@@ -0,0 +1,23 @@
+// Defining a Number Datatype in TS.
+var myInteger = 7;
+var negative = -8;
+var myFloat = 8.657;
+var zero = 0;
+var infinity = Infinity;
+var negativeInfinity = -Infinity;
+console.log(myInteger + myFloat);
+// Defining a String Data Type in TS.
+var firstName = "John";
+var secondName = "Doe";
+var email = "John Doe";
+var fullName = "".concat(firstName, " ").concat(secondName);
+console.log(fullName);
+//Boolean Data Types
+var isTrue = true;
+var isFalse = false;
+var newName = true;
+console.log(newName);
+// Here, null can be assigned to any data type
+var result = null;
+// result = 5;
+// result = 'hello';
---
+++ Tests/index.ts
@@ -0,0 +1,33 @@
+// Defining a Number Datatype in TS.
+
+const myInteger: number = 7;
+const negative: number = -8;
+const myFloat: number = 8.657;
+const zero: number = 0;
+const infinity: number = Infinity;
+const negativeInfinity: number = -Infinity;
+
+console.log(myInteger + myFloat);
+
+// Defining a String Data Type in TS.
+
+const firstName: string = "John";
+const secondName: string = "Doe";
+const email: string = "John Doe";
+const fullName: string = `${firstName} ${secondName}`;
+
+console.log(fullName);
+
+//Boolean Data Types
+
+const isTrue: boolean = true;
+const isFalse: boolean = false; 
+const newName: boolean = true;
+
+console.log(newName);
+
+// Here, null can be assigned to any data type
+
+let result: null = null;
+// result = 5;
+// result = 'hello';
