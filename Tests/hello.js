93a339d9638eecbb4b6aeed489907a5b8a07923c
FrancisMurayaDev <francismurayadev@gmail.com>
2025-03-15 11:22:08 +0300

    running the first typescript code

---
+++ Tests/hello.js
@@ -0,0 +1,4 @@
+// First TypeScript Code 
+var number1 = 67;
+var number2 = 53;
+console.log(number1 + number2);
---
+++ Tests/hello.ts
@@ -0,0 +1,6 @@
+// First TypeScript Code 
+
+let number1: number = 67;
+let number2: number = 53;
+
+console.log(number1 + number2);
